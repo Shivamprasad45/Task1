e592bc808a1968088e974545865d5c2c0008bac8
Shivam gond <codewithharry35434@gmail.com>
2025-07-15 10:59:23 +0530

    ss

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'views', 'index.html'), (err, data) => {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  } else if (req.url === '/message' && req.method === 'POST') {
+    const body = [];
+
+    req.on('data', chunk => {
+      body.push(chunk);
+    });
+
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1].replace(/\+/g, ' ');
+      fs.writeFile('message.txt', message, err => {
+        res.writeHead(302, { Location: '/' });
+        res.end();
+      });
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Page not found');
+  }
+});
+
+server.listen(3000, () => console.log('Server running on http://localhost:3000'));

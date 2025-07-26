9859ef86ee923a6b6dea9fe95f03d1c0406364d6
Shivam gond <codewithharry35434@gmail.com>
2025-07-26 14:24:54 +0530

    ss

--- server.js
+++ server.js
@@ -29,4 +29,4 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => console.log('Server running on http://localhost:3000'));
+server.listen(3000, () => console.log('Server running   on http://localhost:3000'));
